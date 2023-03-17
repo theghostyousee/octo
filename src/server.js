3723500ad212564fe65cc1a0f10b1f658a63bfba
theghostyousee <patrickserise174@gmail.com>
2023-03-17 18:30:58 +0100

    m

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
+// Parse JSON body requests
 app.use(express.json());
+
+// Enable CORS for all origins
 app.use(cors());
 
+// Connect to MongoDB database
 const MONGODB_URI =
-  "mongodb+srv://topezmario8:koussi@cluster0.bdkhw0v.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://topezmario8:koussi@cluster0.bdkhw0v.mongodb.net/test?retryWrites=true&w=majority";
 const DB_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose
@@ -20,6 +24,7 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
+// Define Counter schema
 const CounterSchema = new mongoose.Schema({
   count: {
     type: Number,
@@ -27,14 +32,16 @@ const CounterSchema = new mongoose.Schema({
   },
 });
 
+// Create Counter model
 const Counter = mongoose.model("Counter", CounterSchema);
 
+// Handle POST requests to /api/updateCounter
 app.post("/api/updateCounter", async (req, res) => {
   try {
     const counter = await Counter.findOneAndUpdate(
       {},
       { $inc: { count: 1 } },
-      { new: true }
+      { new: true, upsert: true }
     );
     console.log("New count value:", counter.count);
     res.json({ success: true });
@@ -44,6 +51,8 @@ app.post("/api/updateCounter", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running");
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
