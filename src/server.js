58da142a0d7d4497f2e5958f8804320aa62bfb32
theghostyousee <patrickserise174@gmail.com>
2023-03-17 17:59:16 +0100

    m

--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const MONGODB_URI = 'mongodb+srv://topezmario8:koussi@cluster0.bdkhw0v.mongodb.net/?retryWrites=true&w=majority';
+const MONGODB_URI =
+  "mongodb+srv://topezmario8:koussi@cluster0.bdkhw0v.mongodb.net/?retryWrites=true&w=majority";
 const DB_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 
-mongoose.connect(MONGODB_URI, DB_OPTIONS)
+mongoose
+  .connect(MONGODB_URI, DB_OPTIONS)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
 
 const CounterSchema = new mongoose.Schema({
@@ -25,12 +27,16 @@ const CounterSchema = new mongoose.Schema({
   },
 });
 
-const Counter = mongoose.model('Counter', CounterSchema);
+const Counter = mongoose.model("Counter", CounterSchema);
 
-app.post('/api/updateCounter', async (req, res) => {
+app.post("/api/updateCounter", async (req, res) => {
   try {
-    const counter = await Counter.findOneAndUpdate({}, { $inc: { count: 1 } }, { new: true });
-    console.log('New count value:', counter.count);
+    const counter = await Counter.findOneAndUpdate(
+      {},
+      { $inc: { count: 1 } },
+      { new: true }
+    );
+    console.log("New count value:", counter.count);
     res.json({ success: true });
   } catch (error) {
     console.error(error);
@@ -39,5 +45,5 @@ app.post('/api/updateCounter', async (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running');
+  console.log("Server is running");
 });
